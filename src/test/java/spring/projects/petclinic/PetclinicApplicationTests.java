11340e1263142052b687b2e7cabbe4065ac107bc
Sina Rafati <sn.rafati@gmail.com>
2020-02-01 14:14:49 +0100

    Initial Project Add

---
+++ src/test/java/spring/projects/petclinic/PetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.projects.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
