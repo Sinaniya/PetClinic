6379eadec19726109a63b602e3f03a9442e818f7
Sina Rafati <sn.rafati@gmail.com>
2020-02-03 21:19:39 +0100

    Adding Services.

---
+++ pet-clinic-data/src/main/java/speing/projects/petclinic/services/PetService.java
@@ -0,0 +1,11 @@
+package speing.projects.petclinic.services;
+
+import speing.projects.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+   Pet findById(Long id);
+    Pet save(Pet owner);
+    Set<Pet> findAll();
+}
