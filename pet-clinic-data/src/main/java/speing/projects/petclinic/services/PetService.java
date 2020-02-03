870e921d37dcd2aeceba803141442ca40fa858a8
Sina Rafati <sn.rafati@gmail.com>
2020-02-03 21:41:26 +0100

    Service Interfaces Refactored. closes #3.

--- pet-clinic-data/src/main/java/speing/projects/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/speing/projects/petclinic/services/PetService.java
@@ -2,10 +2,6 @@ package speing.projects.petclinic.services;
 
 import speing.projects.petclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet,Long> {
 
-public interface PetService {
-   Pet findById(Long id);
-    Pet save(Pet owner);
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/speing/projects/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/speing/projects/petclinic/services/VetService.java
@@ -3,10 +3,6 @@ package speing.projects.petclinic.services;
 
 import speing.projects.petclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet,Long>{
 
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
 }
